eb0dbc0d18a4ac55492979d13a03413c2e69ccdb
HadarPX <hadar.tal@perimeterx.com>
2022-05-14 20:13:26 +0300

    wip

--- crawler.py
+++ crawler.py
@@ -1,8 +1,11 @@
+from urllib.parse import unquote
 import requests
 import lxml.html
 import logging
 import json
 import re
+import rdflib
+
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -12,9 +15,74 @@ CAPITAL = 'capital'
 GOVERNMENT = 'government'
 AREA = 'area'
 POPULATION = 'population'
-COUNTRY_KEYS = [CAPITAL, GOVERNMENT, AREA, POPULATION]
+PRESIDENT = 'president'
+COUNTRY_KEYS = [CAPITAL, GOVERNMENT, AREA, POPULATION, PRESIDENT]
+NAME = 'name'
+WHEN_BORN = 'when_born'
+WHERE_BORN = 'where_born'
+PRESIDENT_KEYS = [NAME, WHEN_BORN, WHERE_BORN]
 
-COUNTRIES_TO_CRAWL = 50
+
+def get_url(suffix):
+    return f"{WIKI_PREFIX}{suffix}"
+
+
+def unquote_u(source):
+    result = source
+    if '%u' in result:
+        result = result.replace('%u', '\\u').decode('unicode_escape')
+    result = unquote(result)
+    return result
+
+
+def get_entity(suffix):
+    return rdflib.URIRef(get_url(unquote_u(suffix)))
+
+
+g = rdflib.Graph()
+
+population_rel = get_entity("/wiki/Population")
+capital_city = get_entity("/wiki/Capital_city")
+government_form = get_entity("/wiki/Government")
+area_rel = get_entity("/wiki/Area")
+president_rel = get_entity("/wiki/President_(government_title)")
+prime_minister_rel = get_entity("/wiki/Prime_minister")
+place_of_birth = get_entity("/wiki/Place_of_birth")
+date_of_birth = get_entity("/wiki/Birthday")
+
+COUNTRIES_TO_CRAWL = 4
+
+
+def index(countries):
+    for country in countries:
+        country_entity = get_entity(country['name'])
+        if CAPITAL in country.keys():
+            for capital in country[CAPITAL]:
+                capital_entity = get_entity(capital)
+                g.add((capital_entity, capital_city, country_entity))
+        if GOVERNMENT in country.keys():
+            for government in country[GOVERNMENT]:
+                government_entity = get_entity(government)
+                g.add((government_entity, government_form, country_entity))
+        # if AREA in country.keys():
+        #     area_entity = rdflib.term.Node(country[AREA]) #TODO fake a url
+        #     g.add((area_entity, area_rel, country_entity))
+        # if POPULATION in country.keys():
+        #     population_entity = rdflib.term.Node(country[POPULATION])
+        #     g.add((population_entity, population_rel, country_entity))
+        if PRESIDENT in country.keys():
+            president = country[PRESIDENT]
+            if NAME in president.keys():
+                president_entity = get_entity(president[NAME])
+                g.add((president_entity, president_rel, country_entity))
+                if WHEN_BORN in president.keys():
+                    when_born_entity = get_entity(president[WHEN_BORN])
+                    g.add((when_born_entity, date_of_birth, president_entity))
+                if WHERE_BORN in president.keys():
+                    where_born_entity = get_entity(president[WHERE_BORN])
+                    g.add((where_born_entity, place_of_birth, president_entity))
+
+    g.serialize("graph.nt", format="nt")
 
 
 def crawl():
@@ -27,6 +95,7 @@ def crawl():
         if i > COUNTRIES_TO_CRAWL:
             break
     print(json.dumps(countries, indent=4))
+    return countries
 
 
 def check_country(country):
@@ -46,6 +115,7 @@ def crawl_country(country):
     country[GOVERNMENT] = get_country_government(infobox)
     country[AREA] = get_country_area(infobox)
     country[POPULATION] = get_country_population(infobox)
+    country[PRESIDENT] = get_country_president(infobox)
 
 
 def get_country_capital(infobox):
@@ -101,6 +171,45 @@ def get_country_population(infobox):
     if match:
         res = match.group(0)
         return res
+    return res
+
+
+def get_country_president(infobox):
+    president = {}
+    text = infobox.xpath("//table//tr//a[contains(.,'President')]/ancestor::th/following-sibling::td[1]/a/@href")
+    if len(text) > 0:
+        president[NAME] = text[0]
+    if NAME not in president.keys():
+        logging.error('not found president')
+        return president
+    crawl_president(president)
+    return president
+
+
+def crawl_president(president):
+    doc = get_html(president[NAME])
+    infobox = doc.xpath("//table[contains(@class,'infobox')]")[0]
+    president[WHEN_BORN] = get_when_born(infobox)
+    president[WHERE_BORN] = get_where_born(infobox)
+
+
+def get_when_born(infobox):
+    text = infobox.xpath("//table//tr[contains(.,'Born')]//span[contains(@class,'bday')]/text()")
+    date = ""
+    if len(text) > 0:
+        # date returned will be a datetime.datetime object. here we are only using the first match.
+        return text[0]
+    return date
+
+
+def get_where_born(infobox):
+    text = infobox.xpath("//table//tr[contains(.,'Born')]//a[contains(@href,'wiki')]/@href")
+    place = ""
+    if 0 < len(text) < 10:
+        return text[len(text)-1]
+    if len(text) > 0: #TODO handle
+        return text[0]
+    return place
 
 
 def get_countries():
@@ -125,11 +234,8 @@ def get_html(suffix):
     return doc
 
 
-def get_url(suffix):
-    return f"{WIKI_PREFIX}{suffix}"
-
-
-crawl()
+countries = crawl()
+index(countries)
 
 # country = {'name': '/wiki/Russia'}
 # crawl_country(country)
